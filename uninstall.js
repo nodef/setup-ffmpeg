21480a5140bc4bafa71367f98289625903ba2897
Subhajit Sahu <wolfram77@gmail.com>
2018-11-09 11:37:22 +0530

    unsetup(): renames files before uninstall

--- uninstall.js
+++ uninstall.js
@@ -6,9 +6,13 @@ function readdirMatch(pth, pat) {
   return fs.readdirSync(pth).filter(nam => pat.test(nam));
 };
 
-// Uninstall "youtubeuploader".
+// Uninstall "ffmpeg".
 function unsetup() {
-  var fil = readdirMatch('.', /youtubeuploader.*/)[0];
-  fs.renameSync(fil, 'youtubeuploader');
+  var ffmpeg = readdirMatch('bin', /ffmpeg.*/)[0];
+  var ffplay = readdirMatch('bin', /ffplay.*/)[0];
+  var ffprobe = readdirMatch('bin', /ffprobe.*/)[0];
+  fs.renameSync(ffmpeg, 'ffmpeg');
+  fs.renameSync(ffplay, 'ffplay');
+  fs.renameSync(ffprobe, 'ffprobe');
 };
 unsetup();
