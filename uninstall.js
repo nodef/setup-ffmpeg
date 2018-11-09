9e2ed3a8fe07913527f0e15134ec21325cc247fa
Subhajit Sahu <wolfram77@gmail.com>
2018-11-09 12:18:05 +0530

    unsetup(): mkdirs manually

--- uninstall.js
+++ uninstall.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 
 // Uninstall "ffmpeg".
 function unsetup() {
-  fs.writeFileSync(path.join(pth, 'ffmpeg'), '');
-  fs.writeFileSync(path.join(pth, 'ffplay'), '');
-  fs.writeFileSync(path.join(pth, 'ffprobe'), '');
+  if(!fs.existsSync('assets')) fs.mkdirSync('assets');
+  if(!fs.existsSync('assets/bin')) fs.mkdirSync('assets/bin');
+  fs.writeFileSync('assets/bin/ffmpeg', '');
+  fs.writeFileSync('assets/bin/ffplay', '');
+  fs.writeFileSync('assets/bin/ffprobe', '');
 };
 unsetup();
