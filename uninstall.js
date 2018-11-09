5fa8ff5a316c3895537ea55d64d94c6766a320d0
Subhajit Sahu <wolfram77@gmail.com>
2018-11-09 12:02:03 +0530

    unsetup(): simply creates necessary files

--- uninstall.js
+++ uninstall.js
@@ -1,19 +1,10 @@
 const fs = require('fs');
 
 
-// Match files matching pattern in path.
-function readdirMatch(pth, pat) {
-  return fs.readdirSync(pth).filter(nam => pat.test(nam));
-};
-
 // Uninstall "ffmpeg".
 function unsetup() {
-  var ffmpeg = readdirMatch('assets/bin', /ffmpeg.*/)[0];
-  var ffplay = readdirMatch('assets/bin', /ffplay.*/)[0];
-  var ffprobe = readdirMatch('assets/bin', /ffprobe.*/)[0];
-  fs.renameSync(ffmpeg, 'ffmpeg');
-  fs.renameSync(ffplay, 'ffplay');
-  fs.renameSync(ffprobe, 'ffprobe');
+  fs.writeFileSync('assets/bin/ffmpeg', '');
+  fs.writeFileSync('assets/bin/ffplay', '');
+  fs.writeFileSync('assets/bin/ffprobe', '');
 };
-try { unsetup(); }
-catch(e) {}
+unsetup();
