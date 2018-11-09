379ef1234f79dfa4ba7fc21abaa393917cb88523
Subhajit Sahu <wolfram77@gmail.com>
2018-11-09 12:03:19 +0530

    unsetup(): simply creates necessary folder

--- uninstall.js
+++ uninstall.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 // Uninstall "ffmpeg".
 function unsetup() {
+  fs.mkdirSync('assets/bin', {recursive: true});
   fs.writeFileSync('assets/bin/ffmpeg', '');
   fs.writeFileSync('assets/bin/ffplay', '');
   fs.writeFileSync('assets/bin/ffprobe', '');
